cea182d396e273b1da3b7cf43ef3515901e54288
JR DevPM <astronomi@gmail.com>
2023-09-05 20:33:24 -0300

    Setting Loading components

--- igniteFleet/App.tsx
+++ igniteFleet/App.tsx
@@ -1,10 +1,22 @@
 import { ThemeProvider } from "styled-components/native"
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto"
 
 import theme from "./src/theme"
 
 import { SignIn } from "./src/screens/SignIn"
+import { Loading } from "./src/components/Loading"
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <SignIn />
