b01f56852b3a1145c6505c5c926b5e1b6dfbe079
JR DevPM <astronomi@gmail.com>
2023-09-04 22:16:37 -0300

    Settings ThemeProvider

--- igniteFleet/App.tsx
+++ igniteFleet/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components/native"
+
+import theme from "./src/theme"
+
 import { SignIn } from "./src/screens/SignIn"
 
 export default function App() {
-  return <SignIn />
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  )
 }
