dabc94a9b6858e4b548897dc2a045bad2d41929a
JR DevPM <astronomi@gmail.com>
2023-09-04 21:08:01 -0300

    First commit

---
+++ igniteFleet/App.tsx
@@ -0,0 +1,5 @@
+import { SignIn } from "./src/screens/SignIn"
+
+export default function App() {
+  return <SignIn />
+}
---
+++ igniteFleet/src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+import { styles } from "./styles"
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Hellow World!!!</Text>
+    </View>
+  )
+}
