4eb6b631b565cfb6bb393ef0746596f3b242bd94
JR DevPM <astronomi@gmail.com>
2023-09-07 19:45:30 -0300

    Setting StatusBar

--- igniteFleet/App.tsx
+++ igniteFleet/App.tsx
@@ -9,6 +9,7 @@ import theme from "./src/theme"
 
 import { SignIn } from "./src/screens/SignIn"
 import { Loading } from "./src/components/Loading"
+import { StatusBar } from "react-native"
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -19,6 +20,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <SignIn />
     </ThemeProvider>
   )
