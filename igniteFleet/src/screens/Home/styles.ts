ef878fc1dfcc695a40a1c052e5e80d3f8bf4f2bb
humbertoromanojr <astronomi@gmail.com>
2023-09-09 10:08:00 -0300

    Created Home screens

---
+++ igniteFleet/src/screens/Home/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native"
+
+export const Container = styled.View`
+  flex: 1;
+`
+
+export const Title = styled.Text`
+  align-items: center;
+  justify-content: center;
+
+  font-size: 26px;
+`
