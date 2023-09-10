ec70a2d5563942137712e0e13bf40dfc88f94474
humbertoromanojr <astronomi@gmail.com>
2023-09-10 13:56:04 -0300

    Setting click CarStatus

--- igniteFleet/src/components/CarStatus/index.tsx
+++ igniteFleet/src/components/CarStatus/index.tsx
@@ -1,13 +1,14 @@
+import { TouchableOpacityProps } from "react-native"
 import { Key, Car } from "phosphor-react-native"
 import { useTheme } from "styled-components"
 
 import { Container, IconBox, Message, TextHighLight } from "./styles"
 
-type Props = {
+type Props = TouchableOpacityProps & {
   licensePlate?: string | null
 }
 
-export function CarStatus({ licensePlate = null }: Props) {
+export function CarStatus({ licensePlate = null, ...rest }: Props) {
   const theme = useTheme()
 
   const Icon = licensePlate ? Key : Car
@@ -17,7 +18,7 @@ export function CarStatus({ licensePlate = null }: Props) {
   const status = licensePlate ? "Chegada" : "Saída"
 
   return (
-    <Container>
+    <Container {...rest}>
       <IconBox>
         <Icon size={32} color={theme.COLORS.BRAND_LIGHT} />
       </IconBox>
