35adc592fe1fdec2842d1e690b6964db14afcd80
humbertoromanojr <astronomi@gmail.com>
2023-09-10 13:39:01 -0300

    Finished CarStatus component

--- igniteFleet/src/components/CarStatus/index.tsx
+++ igniteFleet/src/components/CarStatus/index.tsx
@@ -1,5 +1,32 @@
-import { Container } from "./styles"
+import { Key, Car } from "phosphor-react-native"
+import { useTheme } from "styled-components"
 
-export function CarStatus() {
-  return <Container></Container>
+import { Container, IconBox, Message, TextHighLight } from "./styles"
+
+type Props = {
+  licensePlate?: string | null
+}
+
+export function CarStatus({ licensePlate = null }: Props) {
+  const theme = useTheme()
+
+  const Icon = licensePlate ? Key : Car
+  const message = licensePlate
+    ? `Veículo ${licensePlate} em uso. `
+    : `Nenhum vepiculo em uso. `
+  const status = licensePlate ? "Chegada" : "Saída"
+
+  return (
+    <Container>
+      <IconBox>
+        <Icon size={32} color={theme.COLORS.BRAND_LIGHT} />
+      </IconBox>
+
+      <Message>
+        {message}
+
+        <TextHighLight>Clique aqui para registrar o {status}</TextHighLight>
+      </Message>
+    </Container>
+  )
 }
--- igniteFleet/src/components/CarStatus/styles.ts
+++ igniteFleet/src/components/CarStatus/styles.ts
@@ -11,3 +11,32 @@ export const Container = styled.View`
   flex-direction: row;
   align-items: center;
 `
+
+export const IconBox = styled.View`
+  width: 77px;
+  height: 77px;
+  border-radius: 7px;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+
+  margin-right: 12px;
+
+  justify-content: center;
+  align-items: center;
+`
+
+export const Message = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_100};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+
+  flex: 1;
+  text-align: justify;
+  textalignvertical: center;
+`
+
+export const TextHighLight = styled.Text`
+  color: ${({ theme }) => theme.COLORS.BRAND_LIGHT};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+`
