eed43fc62607c8d6f7765556b353927b056a62f8
humbertoromanojr <astronomi@gmail.com>
2023-09-18 20:16:01 -0300

    Setting Header component

--- igniteFleet/src/screens/Departure/index.tsx
+++ igniteFleet/src/screens/Departure/index.tsx
@@ -1,5 +1,10 @@
+import { Header } from "../../components/Header"
 import { Container } from "./styles"
 
 export function Departure() {
-  return <Container></Container>
+  return (
+    <Container>
+      <Header title="Saída" />
+    </Container>
+  )
 }
