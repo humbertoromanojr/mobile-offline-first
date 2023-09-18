9b804f59cbce345f5bdb6319bd9f3c69728cdb24
humbertoromanojr <astronomi@gmail.com>
2023-09-18 20:15:26 -0300

    Finished Header component

--- igniteFleet/src/components/Header/index.tsx
+++ igniteFleet/src/components/Header/index.tsx
@@ -1,7 +1,27 @@
-import React from "react"
+import { TouchableOpacity } from "react-native"
+import { ArrowLeft } from "phosphor-react-native"
+import { useTheme } from "styled-components"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { useNavigation } from "@react-navigation/native"
 
-import { Container } from "./styles"
+import { Container, Title } from "./styles"
 
-export function Header() {
-  return <Container></Container>
+type Props = {
+  title: string
+}
+
+export function Header({ title }: Props) {
+  const { COLORS } = useTheme()
+  const insets = useSafeAreaInsets()
+  const { goBack } = useNavigation()
+
+  return (
+    <Container style={{ paddingTop: insets.top + 42 }}>
+      <TouchableOpacity activeOpacity={0.7} onPress={goBack}>
+        <ArrowLeft size={24} weight="bold" color={COLORS.BRAND_LIGHT} />
+      </TouchableOpacity>
+
+      <Title>{title}</Title>
+    </Container>
+  )
 }
