90ce5f95682fa7ff8d491ee0a9b48f8297c7cd45
humbertoromanojr <astronomi@gmail.com>
2023-09-18 20:10:37 -0300

    Setting entire app with backgroundColor

--- igniteFleet/App.tsx
+++ igniteFleet/App.tsx
@@ -29,7 +29,9 @@ export default function App() {
   return (
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
-        <SafeAreaProvider>
+        <SafeAreaProvider
+          style={{ flex: 1, backgroundColor: theme.COLORS.GRAY_800 }}
+        >
           <StatusBar
             barStyle="light-content"
             backgroundColor="transparent"
