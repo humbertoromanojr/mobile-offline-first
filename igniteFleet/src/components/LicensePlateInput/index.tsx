f235df856277fd51005a9b85a06a162d21c01b66
humbertoromanojr <astronomi@gmail.com>
2023-09-18 21:38:03 -0300

    Finished LicensePlateInput component

--- igniteFleet/src/components/LicensePlateInput/index.tsx
+++ igniteFleet/src/components/LicensePlateInput/index.tsx
@@ -1,5 +1,25 @@
-import { Container } from "./styles"
+import { TextInputProps } from "react-native"
+import { useTheme } from "styled-components"
 
-export function LicensePlateInput() {
-  return <Container></Container>
+import { Container, Input, Label } from "./styles"
+
+type Props = TextInputProps & {
+  label: string
+}
+
+export function LicensePlateInput({ label, ...rest }: Props) {
+  const { COLORS } = useTheme()
+
+  return (
+    <Container>
+      <Label>{label}</Label>
+
+      <Input
+        maxLength={7}
+        autoCapitalize="characters"
+        placeholderTextColor={COLORS.GRAY_400}
+        {...rest}
+      />
+    </Container>
+  )
 }
