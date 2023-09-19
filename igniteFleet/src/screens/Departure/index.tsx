2fd6c081974bfe0a40bf2304a827c95b8eca9a97
humbertoromanojr <astronomi@gmail.com>
2023-09-18 21:38:52 -0300

    Setting LicensePlateInput component

--- igniteFleet/src/screens/Departure/index.tsx
+++ igniteFleet/src/screens/Departure/index.tsx
@@ -1,10 +1,16 @@
 import { Header } from "../../components/Header"
-import { Container } from "./styles"
+import { LicensePlateInput } from "../../components/LicensePlateInput"
+
+import { Container, Content } from "./styles"
 
 export function Departure() {
   return (
     <Container>
       <Header title="Saída" />
+
+      <Content>
+        <LicensePlateInput label="Place do veículo" placeholder="BRA4321" />
+      </Content>
     </Container>
   )
 }
--- igniteFleet/src/screens/Departure/styles.ts
+++ igniteFleet/src/screens/Departure/styles.ts
@@ -5,3 +5,9 @@ export const Container = styled.View`
 
   background-color: ${({ theme }) => theme.COLORS.GRAY_800};
 `
+export const Content = styled.View`
+  flex: 1;
+  gap: 16px;
+  padding: 32px;
+  margin-top: 16px;
+`
