cb5d9c5da05464288e6641e852c9aa9e4faec195
Humberto Junior <astronomi@gmail.com>
2024-04-11 14:33:05 -0300

    Create screen SignIn and styles

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!!!</Text>
+    </View>
+  );
+}
