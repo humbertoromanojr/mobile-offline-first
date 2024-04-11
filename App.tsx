9e7d941590330c7dd953475512e40404aa75245d
Humberto Junior <astronomi@gmail.com>
2024-04-11 15:42:52 -0300

    Setting fontLoaded and StatusBar

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
 import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import theme from "./src/theme";
 
+import { Loading } from "./src/components/Loading";
+
 import { SignIn } from "./src/screens/SignIn";
+import { StatusBar } from "react-native";
 
 export default function App() {
+  const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontLoaded) {
+    return <Loading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <SignIn />;
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn />
     </ThemeProvider>
   );
 }
