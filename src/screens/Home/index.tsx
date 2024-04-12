72cced037b84ab7a0f5989dce577ac7046e03ba5
Humberto Junior <astronomi@gmail.com>
2024-04-12 17:29:53 -0300

    Import and Use HomeHeader component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,13 @@
-//import { useNavigation } from "@react-navigation/native"
+import { useNavigation } from "@react-navigation/native";
 
-import { View, Text } from "react-native";
 import { Container, Content } from "./styles";
 
-//import { HomeHeader } from "../../components/HomeHeader"
-//import { CarStatus } from "../../components/CarStatus"
+import { HomeHeader } from "../../components/HomeHeader";
 
 export function Home() {
-  //const { navigate } = useNavigation()
-
-  function handleRegisterMovement() {
-    //navigate("departure")
-  }
-
   return (
     <Container>
-      <View>
-        <Text>teste</Text>
-      </View>
+      <HomeHeader />
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,12 +1,12 @@
-import styled from "styled-components/native"
+import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
 
   background: ${({ theme }) => theme.COLORS.GRAY_800};
-`
+`;
 
 export const Content = styled.View`
   flex: 1;
   padding: 12px;
-`
+`;
