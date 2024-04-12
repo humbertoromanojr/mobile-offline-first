0429012681de5430f3c7a3eadf77819498614293
Humberto Junior <astronomi@gmail.com>
2024-04-12 10:55:46 -0300

    Create Home screen and styles

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,23 @@
+//import { useNavigation } from "@react-navigation/native"
+
+import { View, Text } from "react-native";
+import { Container, Content } from "./styles";
+
+//import { HomeHeader } from "../../components/HomeHeader"
+//import { CarStatus } from "../../components/CarStatus"
+
+export function Home() {
+  //const { navigate } = useNavigation()
+
+  function handleRegisterMovement() {
+    //navigate("departure")
+  }
+
+  return (
+    <Container>
+      <View>
+        <Text>teste</Text>
+      </View>
+    </Container>
+  );
+}
