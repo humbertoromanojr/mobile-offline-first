1076ffa9f1b1194c91579d98dd8fe13d0a7c6ef3
Humberto Junior <astronomi@gmail.com>
2024-04-12 10:58:13 -0300

    Setting AppProvider to Realm

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import { ThemeProvider } from "styled-components/native";
+import { AppProvider, UserProvider } from "@realm/react";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { StatusBar } from "react-native";
 
-import theme from "./src/theme";
+// screens
+import { SignIn } from "./src/screens/SignIn";
+import { Home } from "./src/screens/Home";
 
+// components
 import { Loading } from "./src/components/Loading";
 
-import { SignIn } from "./src/screens/SignIn";
-import { StatusBar } from "react-native";
+// settings
+import { REALM_APP_ID } from "@env";
+import theme from "./src/theme";
 
 export default function App() {
   const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,13 +26,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
