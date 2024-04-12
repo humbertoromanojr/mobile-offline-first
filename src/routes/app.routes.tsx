6c356b4a1de1c43d1081e08e41469f58b593a110
Humberto Junior <astronomi@gmail.com>
2024-04-12 17:27:10 -0300

    Create Routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+import { Home } from "../screens/Home";
+//import { Departure } from "../screens/Departure"
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="home" component={Home} />
+      {/*<Screen name="departure" component={Departure} />*/}
+    </Navigator>
+  );
+}
