e99b248ff02414013d306d29c9f43756c590809f
Humberto Junior <astronomi@gmail.com>
2024-04-12 17:30:56 -0300

    Create CarStatus component

---
+++ src/components/CarStatus/index.tsx
@@ -0,0 +1,33 @@
+import { TouchableOpacityProps } from "react-native"
+import { Key, Car } from "phosphor-react-native"
+import { useTheme } from "styled-components"
+
+import { Container, IconBox, Message, TextHighLight } from "./styles"
+
+type Props = TouchableOpacityProps & {
+  licensePlate?: string | null
+}
+
+export function CarStatus({ licensePlate = null, ...rest }: Props) {
+  const theme = useTheme()
+
+  const Icon = licensePlate ? Key : Car
+  const message = licensePlate
+    ? `Veículo ${licensePlate} em uso. `
+    : `Nenhum vepiculo em uso. `
+  const status = licensePlate ? "Chegada" : "Saída"
+
+  return (
+    <Container {...rest}>
+      <IconBox>
+        <Icon size={32} color={theme.COLORS.BRAND_LIGHT} />
+      </IconBox>
+
+      <Message>
+        {message}
+
+        <TextHighLight>Clique aqui para registrar o {status}</TextHighLight>
+      </Message>
+    </Container>
+  )
+}
