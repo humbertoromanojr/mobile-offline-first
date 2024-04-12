aa1c0f55023116bbba76fb865117cb4fb94d938a
Humberto Junior <astronomi@gmail.com>
2024-04-12 17:45:48 -0300

    Import and Use SafeAreaProvider

--- App.tsx
+++ App.tsx
@@ -5,11 +5,11 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "react-native";
 
 // screens
 import { SignIn } from "./src/screens/SignIn";
-import { Home } from "./src/screens/Home";
 
 // components
 import { Loading } from "./src/components/Loading";
@@ -31,14 +31,16 @@ export default function App() {
   return (
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <UserProvider fallback={SignIn}>
-          <Routes />
-        </UserProvider>
+        <SafeAreaProvider>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <UserProvider fallback={SignIn}>
+            <Routes />
+          </UserProvider>
+        </SafeAreaProvider>
       </ThemeProvider>
     </AppProvider>
   );
