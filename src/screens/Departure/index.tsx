eea7e7a97a2fa9b9d235b5d8924848187f5598b6
Humberto Junior <astronomi@gmail.com>
2024-04-15 16:44:06 -0300

    Setting KeyboardAvoidingView and ScrollView

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,5 +1,10 @@
 import { useRef } from "react";
-import { TextInput } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+} from "react-native";
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
@@ -7,6 +12,9 @@ import { TextAreaInput } from "../../components/TextAreaInput";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
 
 import { Container, Content } from "./styles";
+
+const KeyboardAvoidingViewBehavior =
+  Platform.OS === "android" ? "height" : "position";
 
 export function Departure() {
   const descriptionRef = useRef<TextInput>(null);
@@ -19,18 +27,25 @@ export function Departure() {
     <Container>
       <Header title="Saída" />
 
-      <Content>
-        <LicensePlateInput
-          label="Placa do veículo"
-          placeholder="BRA4321"
-          onSubmitEditing={() => descriptionRef.current?.focus()}
-          returnKeyType="next"
-        />
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={KeyboardAvoidingViewBehavior}
+      >
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Content>
+            <LicensePlateInput
+              label="Placa do veículo"
+              placeholder="BRA4321"
+              onSubmitEditing={() => descriptionRef.current?.focus()}
+              returnKeyType="next"
+            />
 
-        <TextAreaInput ref={descriptionRef} label="Finalidade" />
+            <TextAreaInput ref={descriptionRef} label="Finalidade" />
 
-        <Button title="Registrar Saída" onPress={handleDepartureRegister} />
-      </Content>
+            <Button title="Registrar Saída" onPress={handleDepartureRegister} />
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
