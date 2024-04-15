cbbfb89d4c46a0fd0d97a8b25ff8444e6d1960dc
Humberto Junior <astronomi@gmail.com>
2024-04-15 14:31:05 -0300

    Create Header component and styles

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import { TouchableOpacity } from "react-native"
+import { ArrowLeft } from "phosphor-react-native"
+import { useTheme } from "styled-components"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { useNavigation } from "@react-navigation/native"
+
+import { Container, Title } from "./styles"
+
+type Props = {
+  title: string
+}
+
+export function Header({ title }: Props) {
+  const { COLORS } = useTheme()
+  const insets = useSafeAreaInsets()
+  const { goBack } = useNavigation()
+
+  return (
+    <Container style={{ paddingTop: insets.top + 42 }}>
+      <TouchableOpacity activeOpacity={0.7} onPress={goBack}>
+        <ArrowLeft size={24} weight="bold" color={COLORS.BRAND_LIGHT} />
+      </TouchableOpacity>
+
+      <Title>{title}</Title>
+    </Container>
+  )
+}
