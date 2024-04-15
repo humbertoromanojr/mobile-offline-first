86de7ddfc7da4905bcdb5d4a159685c7809c8883
Humberto Junior <astronomi@gmail.com>
2024-04-15 15:48:41 -0300

    Create TextAreaInput component and styles

---
+++ src/components/TextAreaInput/index.tsx
@@ -0,0 +1,25 @@
+import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components/native";
+
+import { Container, Label, Input } from "./styles";
+
+type Props = TextInputProps & {
+  label: string;
+};
+
+export function TextAreaInput({ label, ...rest }: Props) {
+  const { COLORS } = useTheme();
+
+  return (
+    <Container>
+      <Label>{label}</Label>
+
+      <Input
+        placeholderTextColor={COLORS.GRAY_400}
+        multiline
+        autoCapitalize="sentences"
+        {...rest}
+      />
+    </Container>
+  );
+}
