3c703f2404a55d5499afdb6fb66d6b37e6713c78
Humberto Junior <astronomi@gmail.com>
2024-04-16 17:44:48 -0300

    Setting route Arrival

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,12 +4,14 @@ const { Navigator, Screen } = createNativeStackNavigator();
 
 import { Home } from "../screens/Home";
 import { Departure } from "../screens/Departure";
+import { Arrival } from "../screens/Arrival";
 
 export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
       <Screen name="departure" component={Departure} />
+      <Screen name="arrival" component={Arrival} />
     </Navigator>
   );
 }
