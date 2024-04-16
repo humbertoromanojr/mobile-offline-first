c5e53ac34d5413376131c64e0ad45a5d59d9dcc6
Humberto Junior <astronomi@gmail.com>
2024-04-16 17:47:07 -0300

    Getting ID from the Home screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useRoute } from "@react-navigation/native";
 
 import { Container } from "./styles";
 
-export default function Arrival() {
+type RouteParamsProps = {
+  id: string;
+};
+
+export function Arrival() {
+  const route = useRoute();
+
+  const { id } = route.params as RouteParamsProps;
+
+  console.log("==> ID: ", id);
+
   return (
     <Container>
       <Text></Text>
