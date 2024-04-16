98e6e23fed32ee7003437b5bef911420b3cfdc8b
Humberto Junior <astronomi@gmail.com>
2024-04-16 17:09:34 -0300

    Update vehicles in use

--- src/components/CarStatus/index.tsx
+++ src/components/CarStatus/index.tsx
@@ -11,7 +11,7 @@ type Props = TouchableOpacityProps & {
 export function CarStatus({ licensePlate = null, ...rest }: Props) {
   const theme = useTheme();
 
-  const Icon = licensePlate ? Key : Car;
+  const Icon = licensePlate ? Car : Key;
   const message = licensePlate
     ? `Veículo ${licensePlate} em uso. `
     : `Nenhum veículo em uso. `;
