cdbcc601a8aed6c1ddd73a1979a310530fd7138d
Humberto Junior <astronomi@gmail.com>
2024-04-16 17:07:19 -0300

    Listing vehicles in use

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,49 @@
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-import { Container, Content } from "./styles";
+import { useQuery } from "../../libs/realm";
+import { Historic } from "../../libs/realm/schemas/Historic";
 
 import { HomeHeader } from "../../components/HomeHeader";
 import { CarStatus } from "../../components/CarStatus";
 
+import { Container, Content } from "./styles";
+
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
+
   const { navigate } = useNavigation();
+
+  const historic = useQuery(Historic);
 
   function handleRegisterMovement() {
     navigate("departure");
   }
+
+  function fetchVehicle() {
+    try {
+      const vehicle = historic.filtered("status = 'departure'")[0];
+      setVehicleInUse(vehicle);
+    } catch (error) {
+      Alert.alert("Vehicle in use", "Vehicle in use could not be loaded!");
+      console.log("==> Error: ", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
 
   return (
     <Container>
       <HomeHeader />
 
       <Content>
-        <CarStatus onPress={handleRegisterMovement} />
+        <CarStatus
+          licensePlate={vehicleInUse?.license_plate}
+          onPress={handleRegisterMovement}
+        />
       </Content>
     </Container>
   );
