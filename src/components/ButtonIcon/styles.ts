5ef11bc63c4302eae00f82b4eeb4dca097adf737
Humberto Junior <astronomi@gmail.com>
2024-04-17 10:28:48 -0300

    Update ButtonIcon component and styles

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -1,7 +1,12 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
-  flex: 1;
+export const Container = styled.TouchableOpacity`
+  width: 56px;
+  height: 56px;
+  border-radius: 6px;
 
-  background: ${({ theme }) => theme.COLORS.GRAY_800};
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 `;
