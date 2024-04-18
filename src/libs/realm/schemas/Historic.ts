4d909744323c7f09243a0a28c0ac2c7fedbf6c10
Humberto Junior <astronomi@gmail.com>
2024-04-18 15:14:17 -0300

    FIx error: static schema: ObjectSchema

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,47 +1,48 @@
-import { Realm } from "@realm/react"
+import { Realm } from "@realm/react";
+import { ObjectSchema } from "realm";
 
 type GenerateProps = {
-  user_id: string
-  description: string
-  license_plate: string
-}
+    user_id: string;
+    description: string;
+    license_plate: string;
+};
 
 export class Historic extends Realm.Object<Historic> {
-  _id!: "uuid"
-  user_id!: "string"
-  license_plate!: "string"
-  description!: "string"
-  status!: "string"
-  created_at!: "date"
-  updated_at!: "date"
+    _id!: string;
+    user_id!: string;
+    license_plate!: string;
+    description!: string;
+    status!: string;
+    created_at!: Date;
+    updated_at!: Date;
 
-  static generate({ user_id, description, license_plate }: GenerateProps) {
-    return {
-      _id: new Realm.BSON.UUID(),
-      user_id,
-      description,
-      license_plate,
-      status: "departure",
-      created_at: new Date(),
-      updated_at: new Date(),
+    static generate({ user_id, description, license_plate }: GenerateProps) {
+        return {
+            _id: new Realm.BSON.UUID(),
+            user_id,
+            license_plate,
+            description,
+            status: "departure",
+            created_at: new Date(),
+            updated_at: new Date(),
+        };
     }
-  }
 
-  static schema = {
-    name: "Historic",
-    primaryKey: "_id",
+    static schema: ObjectSchema = {
+        name: "Historic",
+        primaryKey: "_id",
 
-    properties: {
-      _id: "uuid",
-      user_id: {
-        type: "string",
-        indexed: true,
-      },
-      license_plate: "string",
-      description: "string",
-      status: "string",
-      created_at: "date",
-      updated_at: "date",
-    },
-  }
+        properties: {
+            _id: "uuid",
+            user_id: {
+                type: "string",
+                indexed: true,
+            },
+            license_plate: "string",
+            description: "string",
+            status: "string",
+            created_at: "date",
+            updated_at: "date",
+        },
+    };
 }
