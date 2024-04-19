41c98b04c8636b8f9287a56a66ae37540867093a
Humberto Junior <astronomi@gmail.com>
2024-04-19 11:03:59 -0300

    Remove vehicle in use and goBack screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { useRoute } from "@react-navigation/native";
+import { Alert } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { X } from "phosphor-react-native";
 
-import { useObject } from "../../libs/realm";
+import { useObject, useRealm } from "../../libs/realm";
 import { Historic } from "../../libs/realm/schemas/Historic";
 import { BSON } from "realm";
 
@@ -27,10 +27,27 @@ export function Arrival() {
     const route = useRoute();
     const { id } = route.params as RouteParamsProps;
 
+    const { goBack } = useNavigation();
+    const realm = useRealm();
     const historic = useObject(
         Historic,
         new BSON.UUID(id) as unknown as string
     );
+
+    function handleRemoveVehicleUsage() {
+        Alert.alert("Cancel", "Cancel the use of the vehicle?", [
+            { text: "Não", style: "cancel" },
+            { text: "sim", onPress: () => removeVehicleUsage() },
+        ]);
+    }
+
+    function removeVehicleUsage() {
+        realm.write(() => {
+            realm.delete(historic);
+        });
+
+        goBack();
+    }
 
     return (
         <Container>
@@ -42,7 +59,7 @@ export function Arrival() {
                 <Description>{historic?.description}</Description>
 
                 <Footer>
-                    <ButtonIcon icon={X} />
+                    <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
                     <Button title="Registrar Chegada" />
                 </Footer>
             </Content>
