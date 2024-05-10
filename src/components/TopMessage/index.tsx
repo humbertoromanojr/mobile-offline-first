334647168b6b4c37f1a96f5094809e3950eb2d95
Humberto Junior <astronomi@gmail.com>
2024-05-10 09:15:55 -0300

    Finished component TopMessage

--- src/components/TopMessage/index.tsx
+++ src/components/TopMessage/index.tsx
@@ -1,5 +1,21 @@
-import { Container } from "./styles";
+import { useTheme } from "styled-components/native";
 
-export function TopMessage() {
-    return <Container></Container>;
+import { IconBoxProps } from "../ButtonIcon";
+import { Container, Title } from "./styles";
+
+type Props = {
+    icon?: IconBoxProps;
+    title: string;
+};
+
+export function TopMessage({ title, icon: Icon }: Props) {
+    const { COLORS } = useTheme();
+
+    return (
+        <Container>
+            {Icon && <Icon size={18} color={COLORS.GRAY_100} />}
+
+            <Title>{title}</Title>
+        </Container>
+    );
 }
