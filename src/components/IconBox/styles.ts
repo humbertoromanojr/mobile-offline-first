908bd8c750374a041a074b12c3ef8a94b1d96839
Humberto Junior <astronomi@gmail.com>
2024-05-20 16:48:19 -0300

    Created variant size style

--- src/components/IconBox/styles.ts
+++ src/components/IconBox/styles.ts
@@ -1,5 +1,26 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
-export const Container = styled.View`
-    width: 100%;
+export type SizeProps = "SMALL" | "NORMAL";
+
+type Props = {
+    size: SizeProps;
+};
+
+const variantSizeStyles = (size: SizeProps) => {
+    return {
+        SMALL: css`
+            width: 32px;
+            height: 32px;
+        `,
+        NORMAL: css`
+            width: 46px;
+            height: 46px;
+        `,
+    }[size];
+};
+
+export const Container = styled.View<Props>`
+    flex: 1;
+
+    ${({ size }) => variantSizeStyles(size)}
 `;
