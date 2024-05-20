167a4fa52f32eb166e3ee3e408da60decc238e3f
Humberto Junior <astronomi@gmail.com>
2024-05-20 16:26:09 -0300

    Finished LocationInfo component and styles

---
+++ src/components/LocationInfo/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Info, Label, Description } from "./styles";
+
+export type LocationInfoProps = {
+    label: string;
+    description: string;
+};
+
+type Props = LocationInfoProps;
+
+export function LocationInfo({ label, description }: Props) {
+    return (
+        <Container>
+            <Info>
+                <Label numberOfLines={1}>{label}</Label>
+                <Description numberOfLines={1}>{description}</Description>
+            </Info>
+        </Container>
+    );
+}
