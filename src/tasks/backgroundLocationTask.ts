a5998548b34e84a3cd6eb14e0693736a700d3691
Humberto Junior <astronomi@gmail.com>
2024-05-23 16:36:55 -0300

    Creating a function to stop the task

--- src/tasks/backgroundLocationTask.ts
+++ src/tasks/backgroundLocationTask.ts
@@ -1,5 +1,10 @@
-import { startLocationUpdatesAsync, Accuracy } from "expo-location";
 import * as TaskManager from "expo-task-manager";
+import {
+    Accuracy,
+    stopLocationUpdatesAsync,
+    startLocationUpdatesAsync,
+    hasStartedLocationUpdatesAsync,
+} from "expo-location";
 
 export const BACKGROUND_TASK_NAME = "location-tracking";
 
@@ -25,6 +30,14 @@ TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
 
 export async function startLocationTask() {
     try {
+        const hasStarted = await hasStartedLocationUpdatesAsync(
+            BACKGROUND_TASK_NAME
+        );
+
+        if (hasStarted) {
+            await stopLocationTask();
+        }
+
         await startLocationUpdatesAsync(BACKGROUND_TASK_NAME, {
             accuracy: Accuracy.Highest,
             distanceInterval: 1,
@@ -32,5 +45,19 @@ export async function startLocationTask() {
         });
     } catch (error) {
         console.log("==> startLocationTask: ", error);
+    }
+}
+
+export async function stopLocationTask() {
+    try {
+        const hasStarted = await hasStartedLocationUpdatesAsync(
+            BACKGROUND_TASK_NAME
+        );
+
+        if (hasStarted) {
+            await stopLocationUpdatesAsync(BACKGROUND_TASK_NAME);
+        }
+    } catch (error) {
+        console.log("==> stopLocationTask: ", error);
     }
 }
