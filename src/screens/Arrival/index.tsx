8707a22fa8281e5ed9c01386feeae501c1fd63c1
Humberto Junior <astronomi@gmail.com>
2024-05-23 16:57:17 -0300

    Stopping a task

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -20,7 +20,9 @@ import {
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { ButtonIcon } from "../../components/ButtonIcon";
+
 import { getLastSyncTimestamp } from "../../libs/asyncStorage/syncStorage";
+import { stopLocationTask } from "../../tasks/backgroundLocationTask";
 
 type RouteParamsProps = {
     id: string;
@@ -56,7 +58,7 @@ export function Arrival() {
         goBack();
     }
 
-    function handleVehicleRegister() {
+    async function handleVehicleRegister() {
         try {
             if (!historic) {
                 Alert.alert(
@@ -64,6 +66,8 @@ export function Arrival() {
                     "We couldn't get the data to register the vehicle's arrival!"
                 );
             }
+
+            await stopLocationTask();
 
             realm.write(() => {
                 historic!.status = "arrival";
