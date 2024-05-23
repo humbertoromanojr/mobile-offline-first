61bbdfce20aaf6c4cd314988f5ff0ce7e007a207
Humberto Junior <astronomi@gmail.com>
2024-05-23 17:17:58 -0300

    Saving coordinates to storage

--- src/libs/asyncStorage/locationStorage.ts
+++ src/libs/asyncStorage/locationStorage.ts
@@ -2,10 +2,24 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const STORAGE_KEY = "@ignitefleet:location";
 
+type LocationProps = {
+    latitude: number;
+    longitude: number;
+    timestamp: number;
+};
+
 export async function getStorageLocations() {
     const storage = await AsyncStorage.getItem(STORAGE_KEY);
 
     const response = storage ? JSON.parse(storage) : [];
 
     return response;
+}
+
+export async function saveStorageLocations(newLocation: LocationProps) {
+    const storage = await getStorageLocations();
+
+    storage.push(newLocation);
+
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(storage));
 }
