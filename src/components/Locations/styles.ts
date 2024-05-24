c2efeb2479bf6807ed87ed872f9c2d5e616dd7ec
Humberto Junior <astronomi@gmail.com>
2024-05-24 16:11:25 -0300

    Finished component

--- src/components/Locations/styles.ts
+++ src/components/Locations/styles.ts
@@ -2,7 +2,13 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     width: 100%;
+`;
 
-    flex-direction: row;
-    align-items: center;
+export const Line = styled.View`
+    height: 64px;
+    width: 1px;
+    margin: -2px;
+    margin-left: 23px;
+    border-width: 1px;
+    border-left-color: ${({ theme }) => theme.COLORS.GRAY_400};
 `;
