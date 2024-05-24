582e650247a1369682b1551ca9f2e6c1a37f11ca
Humberto Junior <astronomi@gmail.com>
2024-05-24 10:16:03 -0300

    Using Coords Schema

--- src/libs/realm/index.ts
+++ src/libs/realm/index.ts
@@ -1,17 +1,7 @@
 import { createRealmContext } from "@realm/react";
 
 import { Historic } from "./schemas/Historic";
-
-/* 
-RealmProvider = Provider itself, share DB all app
-useRealm = Instance use Add, delete, update and list
-useQuery = use Searching DB
-useObject = get specific object
-*/
-export const { RealmProvider, useRealm, useQuery, useObject } =
-    createRealmContext({
-        schema: [Historic],
-    });
+import { Coords } from "./schemas/Coords";
 
 const realmAccessBehavior: Realm.OpenRealmBehaviorConfiguration = {
     type: Realm.OpenRealmBehaviorType.OpenImmediately,
@@ -22,3 +12,15 @@ export const syncConfig: any = {
     newRealmFileBehavior: realmAccessBehavior,
     existingRealmFileBehavior: realmAccessBehavior,
 };
+
+/* 
+RealmProvider = Provider itself, share DB all app
+useRealm = Instance use Add, delete, update and list
+useQuery = use Searching DB
+useObject = get specific object
+*/
+
+export const { RealmProvider, useRealm, useQuery, useObject } =
+    createRealmContext({
+        schema: [Historic, Coords],
+    });
